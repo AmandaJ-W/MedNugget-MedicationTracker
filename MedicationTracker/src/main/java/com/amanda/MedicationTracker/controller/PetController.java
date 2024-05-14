e56bb0e3f210ba0d6f119457c15545b69b82f3ac
AmandaJ-W <amandajw2243@outlook.com>
2024-05-14 17:35:11 -0400

    medication dao updates

--- MedicationTracker/src/main/java/com/amanda/MedicationTracker/controller/PetController.java
+++ MedicationTracker/src/main/java/com/amanda/MedicationTracker/controller/PetController.java
@@ -19,32 +19,38 @@ public class PetController {
         this.petService = petService;
     }
 
+    // List all pets
     @GetMapping
     public List<Pet> getAllPets() {
         return petService.listAllPets();
     }
 
+    // Find specific pet by ID
     @GetMapping("/{petId}")
     public Pet getPetById(@PathVariable int petId) {
         return petService.getPetById(petId);
     }
 
+    // Add a pet (pet object)
     @PostMapping("/addPet")
     public Pet addPet(@RequestBody Pet pet) {
         return petService.addPet(pet);
     }
 
+    // Update pet info
     @PutMapping("/update/{petId}")
     public Pet updatePet(@PathVariable int petId, @RequestBody Pet updatedPet) {
         updatedPet.setPetId(petId);
         return petService.updatePet(updatedPet);
     }
 
+    // Remove a pet
     @DeleteMapping("/delete/{petId}")
     public int deletePet(@PathVariable int petId) {
         return petService.deletePet(petId);
     }
 
+    // Find a pet by searching a medication name
     @GetMapping("/petsByMedName")
     public List<Pet> findPetsByMedicationName(@RequestParam String medName){
         return petService.findPetsByMedicationName(medName);
